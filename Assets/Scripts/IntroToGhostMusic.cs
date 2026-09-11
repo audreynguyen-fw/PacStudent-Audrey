1a35757193269ffa0f1790c4be6e9ef8b26b124d
Audrey <nguyenlinhnhi28082007@gmail.com>
2026-09-11 10:35:23 +1000

    Add AuidoManager with intro-to-loop music logic

--- Assets/Scripts/IntroToGhostMusic.cs
+++ Assets/Scripts/IntroToGhostMusic.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class IntroToGhostMusic : MonoBehaviour
 {
-    [SerializeField] private 
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip firstClip;
+    [SerializeField] private AudioClip loopingClip;
+    [SerializeField] private float maxWaitSeconds = 3f;
 
 // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audioSource.clip = firstClip;
+        audioSource.loop = false;
+        audioSource.Play();
         
+        float waitTime = Mathf.Min(firstClip.length, maxWaitSeconds);
+        
+        StartCoroutine(SwitchToLoopAfter(waitTime));
     }
+
+    private IEnumerator SwitchToLoopAfter(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        audioSource.clip = loopingClip;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+    
 
     // Update is called once per frame
     void Update()
